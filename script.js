b2deab56095a5b454d40c0665d103aec92fd8f80
Justin Murphy <j.murphyjt@gmail.com>
2019-06-26 11:31:34 -0700

    upload for shopping list project

--- script.js
+++ script.js
@@ -15,7 +15,23 @@ function handleItemForm(event, formRef) {
 		event.preventDefault();
 	}
 
-	console.log("Event: ", event);
+	let itemHtml = addItemToShoppingList();
+	let itemListRef = document.getElementById("shopping-list");
+	itemListRef.insertAdjacentHTML("afterend", itemHtml);
 
 	return false;
+}
+
+function addItemToShoppingList() {
+	let itemName = document.getElementById("item-name");
+	let itemAmount = document.getElementById("item-amount");
+
+
+}
+
+function createListItemHtml() {
+	return `<li>
+					Item Name - Amount
+			  </li>
+	`;
 }
